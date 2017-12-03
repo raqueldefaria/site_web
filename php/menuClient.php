0ea0ab3812a65a120042eee232ce4b717daf7856
Raquel De Faria <racheldf19@gmail.com>
2017-12-03 23:22:43 +0100

    Ajout de la page Piece - espace Client

---
+++ php/menuClient.php
@@ -0,0 +1,6 @@
+<nav class="menuClient">
+    <a class="homeControl">Home Control</a>
+    <a href="#" class="lienAlarme"> Controle de l'alarme</a>
+    <a href="#" class="lienPieces"> Pieces</a>
+    <a href="#" class="lienCapteurs"> Capteurs / actionneurs</a>
+</nav>
