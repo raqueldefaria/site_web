8d9fb01ce73a3f7d058bb9bf55d65d08e73cd007
remy787 <remy.touret@gmail.com>
2017-12-04 17:03:23 +0100

    Notre équipe

---
+++ php/notreEquipe.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../css/notreEquipe.css" />
+        <link rel="stylesheet" href="../css/header.css" />
+        <link rel="stylesheet" href="../css/footer.css" />
+        <title>DomOnline</title>
+    </head>
+
+
+    <body>
+
+
+    <?php include("header.php"); ?>
+
+    <!-- Le corps -->
+
+        <div id="conteneur1">
+
+            <div class="element"><img src="../images/equipe/Dejous.png"><p>Alexandre Dejous </p></div>
+            <div class="element"><img src="../images/equipe/paresseux.png"><p>Alexandre Dejous </p></div>
+            <div class="element"><img src="../images/equipe/Raquel.png"><p>Alexandre Dejous </p></div>
+        </div>
+
+        <br />
+        <br />
+        <br />
+    <div id="conteneur2">
+                <div class="element"><img src="../images/equipe/Linda.png"><p>Alexandre Dejous </p></div>
+                <div class="element"><img src="../images/equipe/BG.png"><p>Alexandre Dejous </p></div>
+                <div class="element"><img src="../images/equipe/Damien.png"><p>Alexandre Dejous </p></div>
+</div>
+
+
+    <?php include("footer.php"); ?>
+
+    </body>
+
+
+
+
+</html>
