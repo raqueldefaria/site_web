2eda984c2eb62b6a0bf84abbe9f125334c2fcf1e
remy787 <remy.touret@gmail.com>
2017-12-05 12:42:40 +0100

    marche pas je sais pas pq

--- php/notreEquipe.php
+++ php/notreEquipe.php
@@ -21,17 +21,17 @@
         <div id="conteneur1">
 
             <div class="element"><img src="../images/equipe/Dejous.png"><p>Alexandre Dejous </p></div>
-            <div class="element"><img src="../images/equipe/paresseux.png"><p>Alexandre Dejous </p></div>
-            <div class="element"><img src="../images/equipe/Raquel.png"><p>Alexandre Dejous </p></div>
+            <div class="element"><img src="../images/equipe/paresseux.png"><p>Benjamin Torchut </p></div>
+            <div class="element"><img src="../images/equipe/Raquel.png"><p>Raquel DeFaria </p></div>
         </div>
 
         <br />
         <br />
         <br />
     <div id="conteneur2">
-                <div class="element"><img src="../images/equipe/Linda.png"><p>Alexandre Dejous </p></div>
-                <div class="element"><img src="../images/equipe/BG.png"><p>Alexandre Dejous </p></div>
-                <div class="element"><img src="../images/equipe/Damien.png"><p>Alexandre Dejous </p></div>
+                <div class="element"><img src="../images/equipe/Linda.png"><p> Linda Deghdegh </p></div>
+                <div class="element"><img src="../images/equipe/BG.png"><p>   Rémy Touret </p></div>
+                <div class="element"><img src="../images/equipe/Damien.png"><p> Damien Deredec </p></div>
 </div>
 
 
