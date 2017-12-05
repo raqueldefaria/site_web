26091288f2275f7a4d839767041853ada1fdde8b
BenjaminTrc <torchut.benjamin@gmail.com>
2017-12-05 23:55:30 +0100

    test

--- php/notreEquipe.php
+++ php/notreEquipe.php
@@ -28,11 +28,11 @@
         <br />
         <br />
         <br />
-    <div id="conteneur2">
+        <div id="conteneur2">
                 <div class="element"><img src="../images/equipe/Linda.png"><p> Linda Deghdegh </p></div>
                 <div class="element"><img src="../images/equipe/BG.png"><p>   Rémy Touret </p></div>
                 <div class="element"><img src="../images/equipe/Damien.png"><p> Damien Deredec </p></div>
-</div>
+        </div>
 
         <figure class="image">
             <a href="../images/equipe/paresseux.png"> <img src="../images/equipe/paresseux.png" id="img" alt="Photo de Benjamin" title="Cliquez pour agrandir" style="width:200px;height:200px" /></a>
