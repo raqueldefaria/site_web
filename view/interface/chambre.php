21f4d29c05e56a952be82cc65fa20f6442a69ee6
Raquel De Faria <racheldf19@gmail.com>
2017-12-09 15:30:22 +0100

    first commit

---
+++ view/interface/chambre.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="../css/chambre.css" />
+    <link rel="stylesheet" href="../css/clientPieces.css" />
+    <link rel="stylesheet" href="../css/header.css" />
+    <link rel="stylesheet" href="../css/footer.css" />
+    <link rel="stylesheet" href="../css/menuClient.css" />
+  </head>
+
+  <body>
+
+  <!--------------- Header --------------->
+  <?php include ("header.php")?>
+
+  <!--------------- Menu --------------->
+  <?php include ("menuClient.php")?>
+
+
+  <!--------------- Navigation --------------->
+  <div class="pieces">
+      <div class="sectionPieces">
+          <p class="motPiece">Chambre</p>
+          <a href="#"><img src="../images/client/question.png" class="questions"></a>
+      </div>
+
+      <div>
+
+      </div>
+  </div>
+
+
+  <!--------------- Footer --------------->
+  <?php include ("footer.php")?>
+  </body>
+</html>
