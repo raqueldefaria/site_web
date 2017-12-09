5a9cedc761cdf42e66b647b879f21217e23d2dd0
Raquel De Faria <racheldf19@gmail.com>
2017-12-09 15:30:10 +0100

    Working header

--- view/interface/header.php
+++ view/interface/header.php
@@ -1,17 +1,25 @@
       <header>
+              <div id="header">
+                <div class="logo">
+                    <img src="../images/logodomonline.png" alt="Logo du site" id="logoCompanyHeader">
+                </div>
 
-              <div class="logo">
-                  <img src="../images/logodomonline.png" alt="Logo du site" id="logoCompanyHeader">
+                <nav class="menu">
+                  <a href="https://www.facebook.com" class="txtHeader1"> Notre equipe </a>
+                  <a href="https://www.facebook.com" class="txtHeader2"> Nos offres </a>
+                  <a href="https://www.facebook.com" class="txtHeader3"> Nous contacter </a>
+                </nav>
+
+                <div class="bouttons">
+                  <div class="identifier">
+                      <a href="https://www.facebook.com" id="boutonidentifier"> Identification </a>
+                  </div>
+                  <div class="seConnecter">
+                      <a href="https://www.facebook.com" id="boutonSeConnecter"> Connexion </a>
+                  </div>
+                </div>
               </div>
 
-              <nav class="menu">
-                <a href="https://www.facebook.com" class="txtHeader1"> Notre equipe </a>
-                <a href="https://www.facebook.com" class="txtHeader2"> Nos offres </a>
-                <a href="https://www.facebook.com" class="txtHeader3"> Nous contacter </a>
-              </nav>
 
-              <div class="seConnecter">
-                  <a href="https://www.facebook.com" id="boutonSeConnecter"> Se connecter </a>
-              </div>
 
       </header>
