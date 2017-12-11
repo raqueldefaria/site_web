4ef9a68cc5686071e681369bc834a233312a06bb
DDeredec <dderedec92@gmail.com>
2017-12-11 14:28:19 +0100

    page NousContacter

--- view/interface/contacter.php
+++ view/interface/contacter.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="../css/accueil.css" />
         <link rel="stylesheet" href="../css/header.css" />
         <link rel="stylesheet" href="../css/footer.css" />
-        
+
         <title>DomOnline - Nous contacter</title>
     </head>
 
@@ -18,6 +18,14 @@
 
     <!-- Le corps -->
     <div id="corps">
+
+      <p>Des questions sur notre application ? <br />Vous avez un soucis dans votre installation ?<br /> Le matériel est tombé en panne ?<br /> Nous sommes là <span class="bold">pour vous aider</span>.</p>
+
+      <h2>Nous contacter par mail</h2>
+      <p> Envoyez-nous un mail à notre adresse : <a href="mailto:domonline@gmail.com">domonline@gmail.com</a>. <br/> Un membre de notre équipe vous répondera le plus rapidement possible.</p>
+
+      <h2>Nous contacer par téléphone</h2>
+      <p>Appelez-nous au 06 66 66 66 66 entre 9h et 20h, ligne vocale ouverte du lundi au samedi. Un membre de notre équipe sera à votre disposition pour répondre à vos questions.</p>
 
     </div>
 
