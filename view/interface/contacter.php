760e77a9a463323f74b49ebaff2e9a0e78009d4b
DDeredec <dderedec92@gmail.com>
2017-12-11 14:55:27 +0100

    lettre oublié + modif padding

--- view/interface/contacter.php
+++ view/interface/contacter.php
@@ -24,7 +24,7 @@
       <h2>Nous contacter par mail</h2>
       <p> Envoyez-nous un mail à notre adresse : <a href="mailto:domonline@gmail.com">domonline@gmail.com</a>. <br/> Un membre de notre équipe vous répondera le plus rapidement possible.</p>
 
-      <h2>Nous contacer par téléphone</h2>
+      <h2>Nous contacter par téléphone</h2>
       <p>Appelez-nous au 06 66 66 66 66 entre 9h et 20h, ligne vocale ouverte du lundi au samedi. Un membre de notre équipe sera à votre disposition pour répondre à vos questions.</p>
 
     </div>
