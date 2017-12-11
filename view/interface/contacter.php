7813975143601e564d71fcfe6fc65e0cbf836832
DDeredec <dderedec92@gmail.com>
2017-12-11 14:38:37 +0100

    ajout d'un .css pour la page NousContacter

--- view/interface/contacter.php
+++ view/interface/contacter.php
@@ -4,7 +4,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="../css/accueil.css" />
+        <link rel="stylesheet" href="../css/contacter.css" />
         <link rel="stylesheet" href="../css/header.css" />
         <link rel="stylesheet" href="../css/footer.css" />
 
