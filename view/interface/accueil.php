515e95b62fd35a9cbccacab8c9c7205c944b19c9
DDeredec <dderedec92@gmail.com>
2017-12-12 11:23:19 +0100

    grosse modif page d'accueil

--- view/interface/accueil.php
+++ view/interface/accueil.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="../css/accueil.css" />
         <link rel="stylesheet" href="../css/header.css" />
         <link rel="stylesheet" href="../css/footer.css" />
-        <title>DomOnline</title>
+        <title>DomOnline - Accueil </title>
     </head>
 
 
@@ -16,15 +16,51 @@
     <?php include("header.php"); ?>
 
     <!-- Le corps -->
-    <div id="corps">
-        <div id="paragraphe_texte">
-            <h1>Mon super site</h1>
-            <p>
-              Bienvenue sur mon super site !<br />
-              Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
-            </p>
-        </div>
-    </div>
+
+        <div id="corps">
+
+            <div id="page_accueil">
+                <section>
+                    <div id="articles">
+                        <article>
+                            <h1> <img src="images/ico_epingle.png" alt="Catégorie voyage" class="ico_categorie"/> NEWS 1 </h1>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
+                        </article>
+
+                        <article>
+                            <h1> <img src="images/ico_epingle.png" alt="Catégorie voyage" class="ico_categorie"/> NEWS 2 </h1>
+
+                            <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem. Quisque mollis ullamcorper diam vel faucibus. Vestibulum sollicitudin facilisis feugiat. Nulla euismod sodales hendrerit. Donec quis orci arcu. Vivamus fermentum magna a erat ullamcorper dignissim pretium nunc aliquam. Aenean pulvinar condimentum enim a dignissim. Vivamus sit amet lectus at ante adipiscing adipiscing eget vitae felis. In at fringilla est. Cras id velit ut magna rutrum commodo. Etiam ut scelerisque purus. Duis risus elit, venenatis vel rutrum in, imperdiet in quam. Sed vestibulum, libero ut bibendum consectetur, eros ipsum ultrices nisl, in rutrum diam augue non tortor. Fusce nec massa et risus dapibus aliquam vitae nec diam.</p>
+                        </article>
+
+                        <article>
+                            <h1> <img src="images/ico_epingle.png" alt="Catégorie voyage" class="ico_categorie"/> NEWS 3 </h1>
+
+                            <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue. Sed auctor fringilla quam quis porttitor. Praesent vitae dignissim magna. Pellentesque quis sem purus, vel elementum mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas consectetur euismod urna. In hac habitasse platea dictumst. Quisque tincidunt porttitor vestibulum. Ut iaculis, lacus at molestie lacinia, ipsum mi adipiscing ligula, vel mollis sem risus eu lectus. Nunc elit quam, rutrum ut dignissim sit amet, egestas at sem.</p>
+                        </article>
+
+                    </div>
+
+                    <aside>
+                        <h1> A propos de DomOnline </h1>
+
+
+                        <p id="photo_zozor"> <img src="images/logodomonline.png" alt="Logo DomOnline"/> </p>
+
+                        <p>Laisse-moi le temps de me présenter: je m'appelle Zozor, je suis né un 23 novembre 2005.</p>
+
+                        <p>Bien maigre, n'est-ce pas ? C'est pourquoi, aujourd'hui, j'ai décidé d'écrire ma biographie afin que vous sachiez qui je suis réellement.</p>
+
+
+                    </aside>
+                </section>
+          </div>
+
+      </div>
+
+
+
 
     <?php include("footer.php"); ?>
 
@@ -32,3 +68,19 @@
 
 
 </html>
+
+
+
+<!--
+<div id="news">
+    <h1>Mon super site</h1>
+    <p>
+      Bienvenue sur mon super site !<br />
+      Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+    </p>
+</div>
+
+<div id="about_domonline">
+
+</div>
+-->
