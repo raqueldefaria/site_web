f5f702291b9d7657e8419387a35c1f1913cfd884
BenjaminTrc <torchut.benjamin@gmail.com>
2017-12-12 14:13:36 +0100

    Amélioration inscription

--- view/interface/inscription.php
+++ view/interface/inscription.php
@@ -21,22 +21,108 @@
   <div class="corps">
     <form method="post" action="../../controller/traitement_inscription.php">
         <p>
-            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-            <label for="mdp">Mot de passe</label> :  <input type="password" name="mdp" id="mdp" /><br />
-            <label for="mdp2">Confirmez votre mot de passe</label> :  <input type="password" name="mdp2" id="mdp" /><br />
-            <label for="type">Type d'utilisateur</label> : <select name="type" id="type">
-              <option value="particulier">Particulier</option>
-              <option value="gestionnaire">Gestionnaire</option>
-              <option value="administrateur">Administrateur</option>
-            </select><br />
-            <label for="prenom">Prenom</label> :  <input type="text" name="prenom" id="prenom" /><br />
-            <label for="nom">Nom</label> :  <input type="text" name="nom" id="nom" /><br />
-            <label for="dateNaissance">Date de naissance</label> :  <input type="date" name="dateNaissance" id="dateNaissance" /><br />
-            <label for="mail">Mail</label> :  <input type="email" name="mail" id="mail" /><br />
-            <label for="adresse">Adresse</label> :  <input type="text" name="adresse" id="adresse" /><br />
-            <label for="codePostal">Code Postal</label> :  <input type="text" name="codePostal" id="codePostal" /><br />
-            <label for="ville">Ville</label> :  <input type="text" name="ville" id="ville" /><br />
-            <label for="pays">Pays</label> :  <input type="text" name="pays" id="pays" /><br />
+            <table>
+              <tr>
+                <td>
+                <label for="pseudo">Pseudo</label> :
+                </td>
+                <td>
+                <input type="text" name="pseudo" id="pseudo" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                <label for="mdp">Mot de passe</label> :
+                </td>
+                <td>
+                <input type="password" name="mdp" id="mdp" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="mdp2">Confirmez votre mot de passe</label> :
+                </td>
+                <td>
+                  <input type="password" name="mdp2" id="mdp" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="type">Type d'utilisateur</label> :
+                </td>
+                <td>
+                  <select name="type" id="type">
+                   <option value="particulier">Particulier</option>
+                   <option value="gestionnaire">Gestionnaire</option>
+                   <option value="administrateur">Administrateur</option>
+                 </select>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="prenom">Prenom</label> :
+                </td>
+                <td>
+                  <input type="text" name="prenom" id="prenom" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="nom">Nom</label> :
+                </td>
+                <td>
+                  <input type="text" name="nom" id="nom" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="dateNaissance">Date de naissance</label> :
+                </td>
+                <td>
+                  <input type="date" name="dateNaissance" id="dateNaissance" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="mail">Mail</label> :
+                </td>
+                <td>
+                  <input type="email" name="mail" id="mail" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="adresse">Adresse</label> :
+                </td>
+                <td>
+                  <input type="text" name="adresse" id="adresse" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="codePostal">Code Postal</label> :
+                </td>
+                <td>
+                  <input type="text" name="codePostal" id="codePostal" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="ville">Ville</label> :
+                </td>
+                <td>
+                  <input type="text" name="ville" id="ville" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label for="pays">Pays</label> :
+                </td>
+                <td>
+                  <input type="text" name="pays" id="pays" />
+                </td>
+              </tr>
+            </table>
             <input type="submit" value="Envoyer" class="boutton" />
         </p>
 
