b6fae2c17dc7fe87fda34502999d39be3ee4a790
DDeredec <dderedec92@gmail.com>
2017-12-13 17:25:30 +0100

    ajout page profil

---
+++ view/interface/profil.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../css/profil.css" />
+        <link rel="stylesheet" href="../css/header.css" />
+        <link rel="stylesheet" href="../css/footer.css" />
+        <title> DomOnline - Profil </title>
+    </head>
+
+
+    <body>
+
+    <?php include("header.php"); ?>
+
+    <!-- Le corps -->
+    <div id="corps">
+
+        <div id="main_block">
+            <h2>Votre profil</h2>
+            <p>
+
+
+            </p>
+
+        </div>
+
+
+    </div>
+
+    <?php include("footer.php"); ?>
+
+    </body>
+
+
+</html>
