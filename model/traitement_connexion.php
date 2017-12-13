6add260783e4d9dae0721a5fe56e33b450e11ad3
DDeredec <dderedec92@gmail.com>
2017-12-13 10:21:34 +0100

    ponctuation

--- model/traitement_connexion.php
+++ model/traitement_connexion.php
@@ -28,7 +28,7 @@ $resultat = $req->fetch();
 
 if (!$resultat)
 {
-  echo 'Mauvais identifiant ou mot de passe !. Veuillez réessayer';
+  echo 'Mauvais identifiant ou mot de passe ! Veuillez réessayer.';
 }
 else
 {
