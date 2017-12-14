c4700daafd01260ce0dec9d2831fcfc25c9a8be8
AlexandreDejous <alex.dejous@gmail.com>
2017-12-14 15:14:53 +0100

    nosOffresV1.2

--- view/interface/nosOffres.php
+++ view/interface/nosOffres.php
@@ -37,6 +37,60 @@
                             <p>Un capteur de luminosité/luminaire</p>
                         </div>
                     </div>
+                    <h3> 20% de réduction </h3>
+                </div>
+                    <h1 id="demoCatalogueParType"> Par type: </h1>
+<!-- Boite Cemac + Fumee -->
+                    <div id="demoCatalogueBoiteCemacFumee">
+                        <div id="demoCatalogueTypeCemac">
+                            <h2>Cemacs</h2>
+                            <div id="demoCatalogueBoiteCemac" class="demoCatalogueBoite">
+                                <img src="../images/Offres/Cemac.jpg" alt="Cemac">
+                                <p>Cemac - XX euros</p>
+                            </div>
+                        </div>
+                        <div id="demoCatalogueTypeFumee">
+                            <h2>Détecteurs de fumée</h2>
+                            <div id="demoCatalogueBoiteFumee" class="demoCatalogueBoite">
+                                <img src="../images/Offres/detecteur_fumee.jpg" alt="Cemac">
+                                <p>Detecteur de fumée - XX euros</p>
+                            </div>
+                        </div>
+                    </div>
+<!-- -->
+
+
+                        <div id="demoCatalogueBoiteCemac">
+                          <div id="demoCatalogueStarterHumiditeTemperature" class="demoCatalogueBoite">
+                              <img src="../images/Offres/detecteur_humidite_temperature.jpg" alt="Détecteur d'humidité et de température">
+                              <p>Un détecteur d'humidité et de température</p>
+                          </div>
+                        </div>
+                        <div id="demoCatalogueBoiteFumee">
+
+                        </div>
+
+
+                    <div id="demoCatalogueStarterBoite">
+                        <h2> ayayaya </h2>
+                        <div id="demoCatalogueStarterBoiteInner">
+                            <div id="demoCatalogueStarterCemac" class="demoCatalogueBoite">
+                                <img src="../images/Offres/Cemac.jpg" alt="Cemac">
+                                <p>Une Cemac</p>
+                            </div>
+                            <div id="demoCatalogueStarterFumee" class="demoCatalogueBoite">
+                                <img src="../images/Offres/detecteur_fumee.jpg" alt="Détecteur de fumée">
+                                <p>Un détecteur de fumée</p>
+                            </div>
+                            <div id="demoCatalogueStarterHumiditeTemperature" class="demoCatalogueBoite">
+                                <img src="../images/Offres/detecteur_humidite_temperature.jpg" alt="Détecteur d'humidité et de température">
+                                <p>Un détecteur d'humidité et de température</p>
+                            </div>
+                            <div id="demoCatalogueStarterLuminositeLuminaire" class="demoCatalogueBoite">
+                                <img src="../images/Offres/capteur_luminosite_luminaire.jpg" alt="Capteur de luminosité/luminaire">
+                                <p>Un capteur de luminosité/luminaire</p>
+                            </div>
+                        </div>
 
 
 
