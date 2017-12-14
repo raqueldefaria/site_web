99b6bd89deeedee024be60134d20ff5d12bb8249
Raquel De Faria <racheldf19@gmail.com>
2017-12-14 20:46:09 +0100

    inscription working

--- controller/inscription.php
+++ controller/inscription.php
@@ -20,52 +20,49 @@ if(!empty($_POST['pseudo']) && !empty($_POST['mdp']) && !empty($_POST['type']) &
     //$reponse = $db->query('SELECT utilisateur_login, utilisateur_mail FROM utilisateur
     //WHERE utilisateur_login='.$pseudo.' OR utilisateur_mail='.$mail ) or die(print_r($db->errorInfo()));
     $reponse = $db->prepare('SELECT utilisateur_login, utilisateur_mail FROM utilisateur
-                             WHERE utilisateur_login=:pseudo OR utilisateur_mail=:mail') or die(print_r($db->errorInfo()));
+                             WHERE utilisateur_login=:pseudo OR utilisateur_mail=:mail');
+
+    //or die(print_r($db->errorInfo()));
 
     $reponse->bindParam(':pseudo', $pseudo);
     $reponse->bindParam(':mail', $mail);
-
 
     $prenom = $_POST['prenom'];
     $nom = $_POST['nom'];
     $pseudo = $_POST['pseudo'];
     $mail = $_POST['mail'];
     //$idUser = $db->lastInsertId();
-    $reponse->execute() or die(print_r($db->errorInfo()));
+    $reponse->execute();
 
-    $donnee = $reponse->fetch() or die(print_r($db->errorInfo()));
+    $donnee = $reponse->fetch();
 
     if (empty($donnee)){  // L'utilisateur n'existe pas dans la base de données, on peut continuer
         $erreur = false; // on n'a pas d'erreur
         // mdp correctement tapé
         if ($_POST['mdp'] != $_POST['mdp2']) {
-            $text = "Vous n'avez pas tapé le meme mot de passe dans les 2 champs";
-            include("../view/interface/inscription_erreur.php");
+              header("Location:../view/interface/inscription_erreur.php?erreur=2");
             $erreur = true; // on a une erreur
             //echo "Vous n'avez pas tapé le même mot de passe dans les 2 champs.";
         }
     }
     else { // utilisateur trouvé dans la base de données
         if($pseudo == $donnee['utilisateur_login']){
-            $text = "Veillez choisir un autre login";
-            include("../view/interface/inscription_erreur.php");
+              header("Location:../view/interface/inscription_erreur.php?erreur=3");
             $erreur = true;
         }
         elseif(strcmp($mail,$donnee['utilisateur_mail'])==0){
-            $text = "Veillez choisir une autre adresse mail";
-            include("../view/interface/inscription_erreur.php");
+              header("Location:../view/interface/inscription_erreur.php?erreur=4");
             $erreur = true;
         }
 
     }
 
     if($erreur == false){
-        include("../model/utilisateurs_inscription.php");
+      include("../model/utilisateurs_inscription.php");
     }
 }
 else{
-    $text = "Vous n'avez pas rempli tout les champs";
-    include("../view/interface/inscription_erreur.php");
+      header("Location:../view/interface/inscription_erreur.php?erreur=1");
 }
 
 // Hachage du mot de passe
