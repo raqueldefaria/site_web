d188ebf22ee2f38f96be823227acc35b4b665eee
DDeredec <dderedec92@gmail.com>
2017-12-14 15:17:43 +0100

    zishzuid

--- model/utilisateurs_inscription.php
+++ model/utilisateurs_inscription.php
@@ -29,10 +29,7 @@ $insertUser->execute(array(
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 23e42a0ce0ca22b10e75a3f2f760671673b96aa5
 //$idUser = $db->query('SELECT id_Utilisateur FROM utilisateur
                       //WHERE utilisateur_prenom='.$prenom.' AND utilisateur_nom='.$nom) or die(print_r($db->errorInfo()));
 
@@ -48,25 +45,16 @@ $insertLogement->bindParam(':ville', $ville);
 $insertLogement->bindParam(':pays', $pays);
 $insertLogement->bindParam(':id', $idUser);
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 23e42a0ce0ca22b10e75a3f2f760671673b96aa5
 
 $adresse = $_POST['adresse'];
 $codePostal = $_POST['codePostal'];
 $ville = $_POST['ville'];
 $pays = $_POST['pays'];
-<<<<<<< HEAD
-$idUser = $db->lastInsertId();
-$insertLogement->execute();
-=======
 
 $idUser = $db->lastInsertId();
 $insertLogement->execute();
 
->>>>>>> 23e42a0ce0ca22b10e75a3f2f760671673b96aa5
 
 
 
