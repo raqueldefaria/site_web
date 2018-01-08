22b1c4c960ecc20a84bb92f51866096a5fc8e289
DDeredec <dderedec92@gmail.com>
2018-01-08 15:58:14 +0100

    pre remplissage inscription

--- controller/inscription.php
+++ controller/inscription.php
@@ -31,16 +31,22 @@ if(!empty(htmlspecialchars($_POST['pseudo'])) AND !empty(htmlspecialchars($_POST
         $erreur = false; // on n'a pas d'erreur
         // mdp correctement tapé
         if (htmlspecialchars($_POST['mdp']) != htmlspecialchars($_POST['mdp2'])) {
+            require("../model/utilisateurs_inscription_erreur.php");
+            pre_fill();
             header("Location:../view/interface/inscription_erreur.php?erreur=2");
             $erreur = true; // on a une erreur : le mot de passe n'a pas bien ete tape
         }
     }
     else { // utilisateur trouvé dans la base de données, on regarde si il a le meme pseudo ou mail
         if($pseudo == $donnee['utilisateur_login']){
+            require("../model/utilisateurs_inscription_erreur.php");
+            pre_fill();
             header("Location:../view/interface/inscription_erreur.php?erreur=3");
             $erreur = true;
         }
         elseif(strcmp($mail,$donnee['utilisateur_mail'])==0){ // strcmp = string comparaison
+            require("../model/utilisateurs_inscription_erreur.php");
+            pre_fill();
             header("Location:../view/interface/inscription_erreur.php?erreur=4");
             $erreur = true;
         }
@@ -52,6 +58,8 @@ if(!empty(htmlspecialchars($_POST['pseudo'])) AND !empty(htmlspecialchars($_POST
     }
 }
 else{ // un ou plusieurs champs n'ont pas ete remplis
+    require("../model/utilisateurs_inscription_erreur.php");
+    pre_fill();
     header("Location:../view/interface/inscription_erreur.php?erreur=1");
 }
 
