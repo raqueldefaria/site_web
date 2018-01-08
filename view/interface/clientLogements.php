f345b8e523b04f495007879ce6191e1c38347a73
AlexandreDejous <alex.dejous@gmail.com>
2018-01-08 15:53:36 +0100

    Ajout d'une page lgemens copie de pièces

---
+++ view/interface/clientLogements.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="../css/clientLogements.css" />
+    <link rel="stylesheet" href="../css/headerbis.css" />
+    <link rel="stylesheet" href="../css/footer.css" />
+    <link rel="stylesheet" href="../css/menuClient.css" />
+
+    <title>DomOnline - Pièces</title>
+  </head>
+
+  <body>
+
+  <!--------------- Header --------------->
+  <?php include ("headerbis.php")?>
+
+  <!--------------- Menu --------------->
+  <?php include ("menuClient.php")?>
+
+
+  <!--------------- Navigation --------------->
+  <div class="logements">
+      <div class="sectionPieces">
+          <p class="motPiece">Pièces</p>
+          <a href="#"><img src="../images/client/question.png" class="questions"></a>
+      </div>
+      <div class="optionPieces">
+          <a href="#" >
+              <div class="section">
+                  <p>Garage</p>
+                  <img src="../images/client/car.png">
+              </div>
+          </a>
+          <a href="chambre.php">
+              <div class="section">
+                  <p>Chambre 1</p>
+                  <img src="../images/client/bed.png">
+              </div>
+          </a>
+          <a href="#">
+              <div class="section">
+                  <p>Chambre 2</p>
+                  <img src="../images/client/bed.png">
+              </div>
+          </a>
+
+          <a href="#">
+              <div class="section">
+                  <p>Chambre 3</p>
+                  <img src="../images/client/bed.png">
+              </div>
+          </a>
+
+          <a href="#">
+              <div class="section">
+                  <p>Salle de bain</p>
+                  <img src="../images/client/bathtub.png">
+              </div>
+          </a>
+
+          <a href="#">
+              <div class="section">
+                  <p>Bureau</p>
+                  <img src="../images/client/desktop.png">
+              </div>
+          </a>
+
+          <a href="#">
+              <div class="section">
+                  <p>Cuisine</p>
+                  <img src="../images/client/cutlery.png">
+              </div>
+          </a>
+
+          <a href="#" >
+              <div class="section">
+                  <img src="../images/client/add.png" class="addButton">
+              </div>
+          </a>
+      </div>
+  </div>
+
+
+  <!--------------- Footer --------------->
+  <?php include ("footer.php")?>
+  </body>
+</html>
