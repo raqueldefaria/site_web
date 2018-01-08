bb0ad355db7399f5e9a011b9b2432b0ed24862af
DDeredec <dderedec92@gmail.com>
2018-01-08 16:56:19 +0100

    mailto

--- view/interface/notreEquipe.php
+++ view/interface/notreEquipe.php
@@ -23,18 +23,18 @@
 
         <div id="conteneur1">
 
-            <div class="element"><img src="../images/equipe/Dejous.png"><p>Alexandre Dejous <span class="email_domonline"><br /><a>Alexandre.Dejous@Domonline.com </a></span></p></div>
-            <div class="element"><img src="../images/equipe/paresseux.png"><p>Benjamin Torchut </p><a> Benjamin.Torchut@Domonline.com </a></div>
-            <div class="element"><img src="../images/equipe/Raquel.png"><p>Raquel DeFaria </p><a> Raquel.DeFaria@Domonline.com </a></div>
+            <div class="element"><img src="../images/equipe/Dejous.png"><p>Alexandre Dejous<br /><span class="email_domonline"><a href="mailto:alexandre.dejous@domonline.com">alexandre.dejous@domonline.com</a></span></p></div>
+            <div class="element"><img src="../images/equipe/paresseux.png"><p>Benjamin Torchut<br /><span class="email_domonline"><a href="mailto:benjamin.torchut@domonline.com">benjamin.torchut@domonline.com</a></span></p></div>
+            <div class="element"><img src="../images/equipe/Raquel.png"><p>Raquel DeFaria<br /><span class="email_domonline"><a href="mailto:raquel.defaria@domonline.com">raquel.defaria@domonline.com</a></p></div>
         </div>
 
         <br />
         <br />
         <br />
         <div id="conteneur2">
-                <div class="element"><img src="../images/equipe/Linda.png"><p> Linda Deghdegh </p> <a>Linda.Deghdegh@Domonline.com </a></div>
-                <div class="element"><img src="../images/equipe/BG.png"><p>   Rémy Touret </p><a>Rémy.Touret@Domonline.com </a></div>
-                <div class="element"><img src="../images/equipe/Damien.png"><p> Damien Deredec </p><a> Damien.Deredec@Domonline.com </a></div>
+                <div class="element"><img src="../images/equipe/Linda.png"><p>Linda Deghdegh<br /><span class="email_domonline"><a href="mailto:linda.deghdegh@domonline.com">linda.deghdegh@domonline.com</a></span></p></div>
+                <div class="element"><img src="../images/equipe/BG.png"><p>Rémy Touret<br /><span class="email_domonline"><a href="mailto:rémy.touret@domonline.com">rémy.touret@domonline.com</a></span></p></div>
+                <div class="element"><img src="../images/equipe/Damien.png"><p>Damien Deredec<br /><span class="email_domonline"><a href="mailto:damien.deredec@domonline.com">damien.deredec@domonline.com</a></span></p></div>
         </div>
 
 
