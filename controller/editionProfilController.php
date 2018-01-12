096e00a69a29894cedfeb6811f5ae0f3663fdb56
BenjaminTrc <torchut.benjamin@gmail.com>
2018-01-12 10:41:58 +0100

    édition du profil

--- controller/editionProfilController.php
+++ controller/editionProfilController.php
@@ -10,5 +10,14 @@ if(!isset($_SESSION['userID']) AND empty($_SESSION['userID']))
   header("Location: connexion.php");
 }
 
+if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpseudo'] != $userinfo['utilisateur_login'])
+{
+  $newpseudo = htmlspecialchars($_POST['newpseudo']);
+  $insertpseudo = $db->prepare('UPDATE utilisateur SET utilisateur_login = ? WHERE id_Utilisateur = ?');
+  $insertpseudo->execute(array($newpseudo, $userinfo['id_Utilisateur']));
+  $_SESSION['pseudo'] = $newpseudo;
+  header('Location: profil.php');
+}
+
 
 ?>
--- view/interface/editionProfilView.php
+++ view/interface/editionProfilView.php
@@ -28,99 +28,103 @@
          <div align="center">
           <h2>Éditer votre profil</h2>
 
-          <table>
-          <tr>
-             <td align="right">
-                <strong> Pseudo </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Pseudo" id="newpseudo" name="newpseudo" value="<?php echo $userinfo['utilisateur_login']; ?>" />
-             </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Mail </strong> :
-             </td>
-             <td>
-               <input type="mail" placeholder="Mail" id="newmail" name="newmail" value="<?php echo $userinfo['utilisateur_mail']; ?>" />
-             </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Prénom </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Prénom" id="newprenom" name="newprenom" value="<?php echo $userinfo['utilisateur_prenom']; ?>" />
-             </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Nom </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Nom" id="newnom" name="newnom" value="<?php echo $userinfo['utilisateur_nom']; ?>" />
-             </td>
-          </tr>
-          <tr>
-            <td align="right">
-               <strong> Adresse </strong> :
-            </td>
-            <td>
-              <input type="text" placeholder="Adresse" id="adresse" name="adresse" value="<?php echo $userinfo['logement_adresse']; ?>" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right">
-               <strong> Ville </strong> :
-            </td>
-            <td>
-              <input type="text" placeholder="Ville" id="ville" name="ville" value="<?php echo $userinfo['logement_ville']; ?>" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right">
-               <strong> Code postal </strong> :
-            </td>
-            <td>
-              <input type="text" placeholder="Code postal" id="codePostal" name="codePostal" value="<?php echo $userinfo['logement_codePostal']; ?>" />
-            </td>
-          </tr>
-          <tr>
-            <td align="right">
-               <strong> Pays </strong> :
-            </td>
-            <td>
-              <input type="text" placeholder="Pays" id="pays" name="pays" value="<?php echo $userinfo['logement_pays']; ?>" />
-            </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Mot de passe actuel </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Mot de passe actuel" id="mdpactuel" name="mdpactuel" />
-             </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Nouveau mot de passe </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Nouveau mot de passe" id="newmdp1" name="newmdp1" />
-             </td>
-          </tr>
-          <tr>
-             <td align="right">
-                <strong> Confirmer nouveau mot de passe </strong> :
-             </td>
-             <td>
-               <input type="text" placeholder="Nouveau mot de passe" id="newmdp2" name="newmdp2" />
-             </td>
-          </tr>
-       </table>
+          <form method="POST" action="editionProfilView.php">
 
-       <br />
-       <input type="submit" value="Mettre à jour mon profil !" class="boutton"/>
+            <table>
+            <tr>
+               <td align="right">
+                  <strong> Pseudo </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Pseudo" id="newpseudo" name="newpseudo" value="<?php echo $userinfo['utilisateur_login']; ?>" />
+               </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Mail </strong> :
+               </td>
+               <td>
+                 <input type="mail" placeholder="Mail" id="newmail" name="newmail" value="<?php echo $userinfo['utilisateur_mail']; ?>" />
+               </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Prénom </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Prénom" id="newprenom" name="newprenom" value="<?php echo $userinfo['utilisateur_prenom']; ?>" />
+               </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Nom </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Nom" id="newnom" name="newnom" value="<?php echo $userinfo['utilisateur_nom']; ?>" />
+               </td>
+            </tr>
+            <tr>
+              <td align="right">
+                 <strong> Adresse </strong> :
+              </td>
+              <td>
+                <input type="text" placeholder="Adresse" id="adresse" name="adresse" value="<?php echo $userinfo['logement_adresse']; ?>" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right">
+                 <strong> Ville </strong> :
+              </td>
+              <td>
+                <input type="text" placeholder="Ville" id="ville" name="ville" value="<?php echo $userinfo['logement_ville']; ?>" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right">
+                 <strong> Code postal </strong> :
+              </td>
+              <td>
+                <input type="text" placeholder="Code postal" id="codePostal" name="codePostal" value="<?php echo $userinfo['logement_codePostal']; ?>" />
+              </td>
+            </tr>
+            <tr>
+              <td align="right">
+                 <strong> Pays </strong> :
+              </td>
+              <td>
+                <input type="text" placeholder="Pays" id="pays" name="pays" value="<?php echo $userinfo['logement_pays']; ?>" />
+              </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Mot de passe actuel </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Mot de passe actuel" id="mdpactuel" name="mdpactuel" />
+               </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Nouveau mot de passe </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Nouveau mot de passe" id="newmdp1" name="newmdp1" />
+               </td>
+            </tr>
+            <tr>
+               <td align="right">
+                  <strong> Confirmer nouveau mot de passe </strong> :
+               </td>
+               <td>
+                 <input type="text" placeholder="Nouveau mot de passe" id="newmdp2" name="newmdp2" />
+               </td>
+            </tr>
+         </table>
+
+         <br />
+         <input type="submit" value="Mettre à jour mon profil !" class="boutton"/>
+
+       </form>
 
        </div>
 
