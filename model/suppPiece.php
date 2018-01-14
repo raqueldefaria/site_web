91a8a5419da8271c62853c7513bcbcbcaf4499ca
AlexandreDejous <alex.dejous@gmail.com>
2018-01-14 18:31:18 +0100

    Bouton supprimer piece fini !

---
+++ model/suppPiece.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$idLogement = $_SESSION['idLogement'];
+
+
+
+
+/* ------------------- BDD ------------------- */
+require("connection_db.php");
+
+
+$db->exec("DELETE FROM piece WHERE id_Piece = ". $_GET['id']." AND Logement_Utilisateur_idUtilisateur = ".$_SESSION['userID']) or die(print_r($db->errorInfo()));
+
+header("Location:../view/interface/clientPieces.php?id=$idLogement");
+//* Supprime l'id de la pièce que l'on veut supprimer (on connait l'id grace à un $_GET)*//
+?>
