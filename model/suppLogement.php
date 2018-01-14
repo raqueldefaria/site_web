2a41acdb18edb9e6b511d2b61d7fb2de0fe825f2
AlexandreDejous <alex.dejous@gmail.com>
2018-01-14 21:15:20 +0100

    Suppr logement full fonctionnelle (pièces à l'int supprimées aussi)

--- model/suppLogement.php
+++ model/suppLogement.php
@@ -8,6 +8,7 @@ session_start();
 require("connection_db.php");
 
 
+$db->exec("DELETE FROM piece WHERE Logement_idLogement = ". htmlspecialchars($_GET['id'])." AND Logement_Utilisateur_idUtilisateur = ".$_SESSION['userID']) or die(print_r($db->errorInfo()));
 $db->exec("DELETE FROM logement WHERE id_Logement = ". htmlspecialchars($_GET['id'])." AND id_Utilisateur = ".$_SESSION['userID']) or die(print_r($db->errorInfo()));
 
 header("Location:../view/interface/logements.php");
