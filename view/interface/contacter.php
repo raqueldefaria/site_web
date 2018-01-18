425fe69b118c44f93cc518f8aac4c8439b01ebbe
DDeredec <dderedec92@gmail.com>
2018-01-18 10:31:43 +0100

    modif mail domonline

--- view/interface/contacter.php
+++ view/interface/contacter.php
@@ -31,7 +31,7 @@ session_start();
         <p>Des questions sur notre application ? <br />Vous avez un soucis dans votre installation ?<br /> Le matériel est tombé en panne ?<br /> Nous sommes là <span class="bold">pour vous aider</span>.</p>
 
         <h2><img src="../images/icone_mail.png" alt"" /> <br />Nous contacter par mail</span></h2>
-        <p> Envoyez-nous un mail à notre adresse : <a href="mailto:domonline@gmail.com">domonline@gmail.com</a>. <br/> Un membre de notre équipe vous répondera dans les plus brefs délais.</p>
+        <p> Envoyez-nous un mail à notre adresse : <a href="mailto:domonline.isep@gmail.com">domonline.isep@gmail.com</a><br/> Un membre de notre équipe vous répondera dans les plus brefs délais.</p>
 
         <h2><img src="../images/logo_telephone.png" alt"" /> <br />Nous contacter par téléphone</h2>
         <p>Appelez-nous au 06 66 66 66 66 entre 9h et 20h, ligne vocale ouverte du lundi au samedi.<br/> Un membre de notre équipe sera à votre disposition pour répondre à vos questions.</p>
