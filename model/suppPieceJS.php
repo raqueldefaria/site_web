5ffb798fa8b0a5dfca85fbd0490773dce1d223f3
AlexandreDejous <alex.dejous@gmail.com>
2018-01-19 11:41:47 +0100

    aaaaa

--- model/suppPieceJS.php
+++ model/suppPieceJS.php
@@ -9,12 +9,18 @@ $idPieceSQL = $obj->idPiece; // j'ai créé une nouvelle variable au lieu d'util
 /* ------------------- BDD ------------------- */
 require("connection_db.php");
 
-$cemac_infoID_raw = $db->query("SELECT DISTINCT id_Cemac FROM cemac WHERE Piece_idPiece =" .$idPieceSQL) or die(print_r($db->errorInfo()));
+$cemac_infoID_raw = $db->query("SELECT id_Cemac FROM cemac WHERE Piece_idPiece = " .$idPieceSQL);// or die(print_r($db->errorInfo()));
 
 while($cemac_infoID = $cemac_infoID_raw->fetch()){
 //on supprime les capteurs ayant l'id cemac de la pièce en question
-    $db->exec("DELETE FROM `capteur/actionneur` WHERE Cemac_idCemac = " .$cemac_infoID['id_Cemac']. " AND Utilisateur_idUtilisateur = " .$_SESSION['userID']) or die(print_r($db->errorInfo()));
+    $db->exec("DELETE FROM `capteur/actionneur` WHERE Cemac_idCemac = " .$cemac_infoID['id_Cemac']);// or die(print_r($db->errorInfo()));
 }
-$db->exec("DELETE FROM cemac WHERE Piece_idPiece = " .$idPieceSQL) or die(print_r($db->errorInfo())); // faudrait mettre la clé utilisateur dans la table Cemac
-$db->exec("DELETE FROM piece WHERE id_Piece = ".$idPieceSQL." AND Logement_Utilisateur_idUtilisateur = ".$_SESSION['userID']) or die(print_r($db->errorInfo()));
+$db->exec("DELETE FROM cemac WHERE Piece_idPiece = " .$idPieceSQL); //or die(print_r($db->errorInfo())); // faudrait mettre la clé utilisateur dans la table Cemac
+$db->exec("DELETE FROM piece WHERE id_Piece = ".$idPieceSQL); //or die(print_r($db->errorInfo()));
 //On supprime également toutes les cemacs se trouvant dans la pièce et enfin la pièce en elle même
+
+
+//$execute = $db->prepare("INSERT INTO piece (piece_nom ,  Logement_idLogement , Logement_Utilisateur_idUtilisateur , piece_type) VALUES (? , ? , ? , ? )") or die(print_r($db->errorInfo()));
+//$execute->execute(array((string)$idPieceSQL , 55 , 82 , "Garage"));
+
+//$db->closeCursor();
