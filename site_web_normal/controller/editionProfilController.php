a9dbc9987d63298afcdebb8b3e84c263a40e14ea
BenjaminTrc <torchut.benjamin@gmail.com>
2018-01-24 12:10:49 +0100

    MVC Edition profil

--- site_web_normal/controller/editionProfilController.php
+++ site_web_normal/controller/editionProfilController.php
@@ -13,67 +13,56 @@ if(!isset($_SESSION['userID']) AND empty($_SESSION['userID']))
 if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpseudo'] != $userinfo['utilisateur_login'])
 {
   $newpseudo = htmlspecialchars($_POST['newpseudo']);
-  $insertpseudo = $db->prepare('UPDATE utilisateur SET utilisateur_login = ? WHERE id_Utilisateur = ?');
-  $insertpseudo->execute(array($newpseudo, $userinfo['id_Utilisateur']));
-  $_SESSION['pseudo'] = $newpseudo;
+  updatepseudo($newpseudo, $userinfo['id_Utilisateur']);
   header('Location: profil?msg=1.php');
 }
 
 if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $userinfo['utilisateur_mail'])
 {
   $newmail = htmlspecialchars($_POST['newmail']);
-  $insertmail = $db->prepare('UPDATE utilisateur SET utilisateur_mail = ? WHERE id_Utilisateur = ?');
-  $insertmail->execute(array($newmail, $userinfo['id_Utilisateur']));
+  updatemail($newmail, $userinfo['id_Utilisateur']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $userinfo['utilisateur_prenom'])
 {
   $newprenom = htmlspecialchars($_POST['newprenom']);
-  $insertprenom = $db->prepare('UPDATE utilisateur SET utilisateur_prenom = ? WHERE id_Utilisateur = ?');
-  $insertprenom->execute(array($newprenom, $userinfo['id_Utilisateur']));
-  $_SESSION['prenom'] = $newprenom;
+  updateprenom($newprenom, $userinfo['id_Utilisateur']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $userinfo['utilisateur_nom'])
 {
   $newnom = htmlspecialchars($_POST['newnom']);
-  $insertnom = $db->prepare('UPDATE utilisateur SET utilisateur_nom = ? WHERE id_Utilisateur = ?');
-  $insertnom->execute(array($newnom, $userinfo['id_Utilisateur']));
-  $_SESSION['nom'] = $newnom;
+  updatenom($newnom, $userinfo['id_Utilisateur']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newadresse']) AND !empty($_POST['newadresse']) AND $_POST['newadresse'] != $userinfo['logement_adresse'])
 {
   $newadresse = htmlspecialchars($_POST['newadresse']);
-  $insertadresse = $db->prepare('UPDATE logement SET logement_adresse = ? WHERE id_Logement = ?');
-  $insertadresse->execute(array($newadresse, $_POST['idlogement']));
+  updateadresse($newadresse, $_POST['idlogement']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newville']) AND !empty($_POST['newville']) AND $_POST['newville'] != $userinfo['logement_ville'])
 {
   $newville = htmlspecialchars($_POST['newville']);
-  $insertville = $db->prepare('UPDATE logement SET logement_ville = ? WHERE id_Logement = ?');
-  $insertville->execute(array($newville, $_POST['idlogement']));
+  updateville($newville, $_POST['idlogement']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newcodePostal']) AND !empty($_POST['newcodePostal']) AND $_POST['newcodePostal'] != $userinfo['logement_codePostal'])
 {
   $newcodePostal = htmlspecialchars($_POST['newcodePostal']);
-  $insertcodePostal = $db->prepare('UPDATE logement SET logement_codePostal = ? WHERE id_Logement = ?');
-  $insertcodePostal->execute(array($newcodePostal, $_POST['idlogement']));
+  updatecodePostal($newcodePostal, $_POST['idlogement']);
   header('Location:  profil?msg=1.php');
 }
 
 if(isset($_POST['newpays']) AND !empty($_POST['newpays']) AND $_POST['newpays'] != $userinfo['logement_pays'])
 {
   $newpays = htmlspecialchars($_POST['newpays']);
-  $insertpays = $db->prepare('UPDATE logement SET logement_pays = ? WHERE id_Logement = ?');
-  $insertpays->execute(array($newpays, $_POST['idlogement']));
+  updatepays($newpays, $_POST['idlogement']);
   header('Location:  profil?msg=1.php');
 }
 
@@ -86,8 +75,7 @@ if(isset($_POST['mdpactuel']) AND !empty($_POST['mdpactuel']) AND isset($_POST['
       if($mdpactuel == $userinfo['utilisateur_motDePasse'] )
       {
         if($newmdp1 == $newmdp2) {
-           $insertmdp = $db->prepare("UPDATE utilisateur SET utilisateur_motDePasse = ? WHERE id_Utilisateur = ?");
-           $insertmdp->execute(array($newmdp1, $userinfo['id_Utilisateur']));
+           updatemdp($newmdp1, $userinfo['id_Utilisateur']);
            header('Location:  profil?msg=1.php');
         }
         else {
--- site_web_normal/model/editionProfilModel.php
+++ site_web_normal/model/editionProfilModel.php
@@ -7,4 +7,71 @@ $requser = $db->prepare('SELECT * FROM utilisateur,logement  WHERE utilisateur.i
 $requser->execute(array($_SESSION['userID'], $_SESSION['userID']));
 $userinfo = $requser->fetch();
 
+
+function updatepseudo($newpseudo, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertpseudo = $db->prepare('UPDATE utilisateur SET utilisateur_login = ? WHERE id_Utilisateur = ?');
+  $insertpseudo->execute(array($newpseudo, $iduser));
+  $_SESSION['pseudo'] = $newpseudo;
+}
+
+function updatemail($newmail, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertmail = $db->prepare('UPDATE utilisateur SET utilisateur_mail = ? WHERE id_Utilisateur = ?');
+  $insertmail->execute(array($newmail, $iduser));
+}
+
+function updateprenom($newprenom, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertprenom = $db->prepare('UPDATE utilisateur SET utilisateur_prenom = ? WHERE id_Utilisateur = ?');
+  $insertprenom->execute(array($newprenom, $iduser));
+  $_SESSION['prenom'] = $newprenom;
+}
+
+function updatenom($newnom, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertnom = $db->prepare('UPDATE utilisateur SET utilisateur_nom = ? WHERE id_Utilisateur = ?');
+  $insertnom->execute(array($newnom, $iduser));
+  $_SESSION['nom'] = $newnom;
+}
+
+function updateadresse($newadresse, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertadresse = $db->prepare('UPDATE logement SET logement_adresse = ? WHERE id_Logement = ?');
+  $insertadresse->execute(array($newadresse, $iduser));
+}
+
+function updateville($newville, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertville = $db->prepare('UPDATE logement SET logement_ville = ? WHERE id_Logement = ?');
+  $insertville->execute(array($newville, $iduser));
+}
+
+function updatecodePostal($newcodePostal, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertcodePostal = $db->prepare('UPDATE logement SET logement_codePostal = ? WHERE id_Logement = ?');
+  $insertcodePostal->execute(array($newcodePostal, $iduser));
+}
+
+function updatepays($newpays, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertpays = $db->prepare('UPDATE logement SET logement_pays = ? WHERE id_Logement = ?');
+  $insertpays->execute(array($newpays, $iduser));
+}
+
+function updatemdp($newmdp, $iduser)
+{
+  require("../../model/connection_db.php");
+  $insertmdp = $db->prepare("UPDATE utilisateur SET utilisateur_motDePasse = ? WHERE id_Utilisateur = ?");
+  $insertmdp->execute(array($newmdp, $iduser));
+}
+
 ?>
