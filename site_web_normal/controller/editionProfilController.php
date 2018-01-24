c24314d6fd4ec91c2d53ba4da8a6bf86344e9a41
BenjaminTrc <torchut.benjamin@gmail.com>
2018-01-24 12:14:37 +0100

    azer

--- site_web_normal/controller/editionProfilController.php
+++ site_web_normal/controller/editionProfilController.php
@@ -14,56 +14,56 @@ if(isset($_POST['newpseudo']) AND !empty($_POST['newpseudo']) AND $_POST['newpse
 {
   $newpseudo = htmlspecialchars($_POST['newpseudo']);
   updatepseudo($newpseudo, $userinfo['id_Utilisateur']);
-  header('Location: profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $userinfo['utilisateur_mail'])
 {
   $newmail = htmlspecialchars($_POST['newmail']);
   updatemail($newmail, $userinfo['id_Utilisateur']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $userinfo['utilisateur_prenom'])
 {
   $newprenom = htmlspecialchars($_POST['newprenom']);
   updateprenom($newprenom, $userinfo['id_Utilisateur']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $userinfo['utilisateur_nom'])
 {
   $newnom = htmlspecialchars($_POST['newnom']);
   updatenom($newnom, $userinfo['id_Utilisateur']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newadresse']) AND !empty($_POST['newadresse']) AND $_POST['newadresse'] != $userinfo['logement_adresse'])
 {
   $newadresse = htmlspecialchars($_POST['newadresse']);
   updateadresse($newadresse, $_POST['idlogement']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newville']) AND !empty($_POST['newville']) AND $_POST['newville'] != $userinfo['logement_ville'])
 {
   $newville = htmlspecialchars($_POST['newville']);
   updateville($newville, $_POST['idlogement']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newcodePostal']) AND !empty($_POST['newcodePostal']) AND $_POST['newcodePostal'] != $userinfo['logement_codePostal'])
 {
   $newcodePostal = htmlspecialchars($_POST['newcodePostal']);
   updatecodePostal($newcodePostal, $_POST['idlogement']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 if(isset($_POST['newpays']) AND !empty($_POST['newpays']) AND $_POST['newpays'] != $userinfo['logement_pays'])
 {
   $newpays = htmlspecialchars($_POST['newpays']);
   updatepays($newpays, $_POST['idlogement']);
-  header('Location:  profil?msg=1.php');
+  header('Location: profil.php?msg=1');
 }
 
 
@@ -76,7 +76,7 @@ if(isset($_POST['mdpactuel']) AND !empty($_POST['mdpactuel']) AND isset($_POST['
       {
         if($newmdp1 == $newmdp2) {
            updatemdp($newmdp1, $userinfo['id_Utilisateur']);
-           header('Location:  profil?msg=1.php');
+           header('Location: profil.php?msg=1');
         }
         else {
            $msg = "Vos deux nouveaux mots de passe ne correspondent pas !";
