d3e3c08c6098fb513f189feb10ce5e535a32bcbf
Raquel De Faria <racheldf19@gmail.com>
2018-01-31 12:46:38 +0100

    MAJ

--- site_web_mvc/model/ajax/deleteSensorJs.php
+++ site_web_mvc/model/ajax/deleteSensorJs.php
@@ -7,7 +7,12 @@ $obj = json_decode($_POST["x"], false);
 /* ------------------- DB ------------------- */
 require("../connectionDb.php");
 
+$db->exec("DELETE FROM `capteur/actionneur` WHERE `id_Capteur/actionneur` = " .$obj->idCapteur);//or die(print_r($db->errorInfo()));
+
+/*
 $existingFailures = $db->query("SELECT * FROM panne WHERE `Capteur/actionneur_idCapteur/actionneur`=".$obj->idCapteur);
+die(print_r($existingFailures));
+
 die(print_r($existingFailures->rowCount()));
 $existingData = $db->query("SELECT * FROM donnees WHERE `Capteur/actionneur_idCapteur/actionneur`=".$obj->idCapteur);
 
@@ -27,8 +32,8 @@ elseif ($existingFailures->rowCount()!=0 AND $existingData->rowCount()==0){
 elseif ($existingFailures->rowCount()!=0 AND $existingData->rowCount()!=0){
     $db->exec("DELETE FROM panne WHERE `Capteur/actionneur_idCapteur/actionneur` = " .$obj->idCapteur);//or die(print_r($db->errorInfo()));
     $db->exec("DELETE FROM donnees WHERE `Capteur/actionneur_idCapteur/actionneur` = " .$obj->idCapteur);//or die(print_r($db->errorInfo()));
-    $db->exec("DELETE FROM `capteur/actionneur` WHERE `id_Capteur/actionneur` = " .$obj->idCapteur);//or die(print_r($db->errorInfo()));
-}
 
+}
+*/
 
 
